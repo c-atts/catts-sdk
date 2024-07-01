670e7cccc67ec60caa32bead796c2a8874e516bd
Kristofer <kristofer@fmckl.se>
2024-07-01 23:54:07 +0200

    first commit

---
+++ catts.ts
@@ -0,0 +1,173 @@
+import "dotenv/config";
+
+import * as fs from "fs";
+import * as path from "path";
+
+import {
+  Recipe,
+  fetchQuery,
+  getSchemaUid,
+  runProcessor,
+  validateSchemaItems,
+} from "./lib";
+
+import { Command } from "commander";
+
+async function importRecipe(recipeFolder: string): Promise<Recipe> {
+  const recipePath = path.join(recipeFolder, "recipe.ts");
+
+  if (!fs.existsSync(recipePath)) {
+    throw new Error(`Recipe file not found: ${recipePath}`);
+  }
+
+  const absolutePath = path.resolve(recipePath);
+  const recipeImport = await import(absolutePath);
+  return recipeImport.default;
+}
+
+// Loads and wraps the processor script for execution in QuickJS VM.
+export async function loadProcessor(recipeFolder: string): Promise<string> {
+  const processorPath = path.join(recipeFolder, "processor.js");
+  return fs.promises.readFile(processorPath, "utf8");
+}
+
+// Unified error logging function.
+function logError(error: unknown): void {
+  if (typeof error === "object" && error !== null) {
+    if ("name" in error) console.log(error.name);
+    if ("message" in error) console.log(error.message);
+  } else {
+    console.error(error);
+  }
+}
+
+type QueryCommandOptions = {
+  index?: number;
+};
+
+function write(text: string) {
+  process.stdout.write(text);
+}
+
+function writeln(text: string) {
+  process.stdout.write(text + "\n");
+}
+
+async function queryCommand(
+  recipeFolder: string,
+  options?: QueryCommandOptions
+) {
+  try {
+    const recipe = await importRecipe(recipeFolder);
+    console.log("\nRecipe:", recipe.name);
+
+    let queryResults;
+    if (options?.index !== undefined) {
+      write(`\nRunning query with index: ${options.index} `);
+      const query = recipe.queries[options.index];
+      queryResults = await fetchQuery(query);
+    } else {
+      write("\nRunning all queries: ");
+      const queryPromises = recipe.queries.map(fetchQuery);
+      queryResults = await Promise.all(queryPromises);
+    }
+
+    writeln("✅\n");
+    console.log(JSON.stringify(queryResults, null, 2));
+  } catch (error) {
+    console.log("\n🛑 Query failed");
+    logError(error);
+  }
+}
+
+async function runCommand(recipeFolder: string) {
+  try {
+    const recipe = await importRecipe(recipeFolder);
+    console.log("\nRecipe:", recipe.name);
+
+    write("\n1/3 Running graphql queries... ");
+    const queryPromises = recipe.queries.map(fetchQuery);
+    const queryResults = await Promise.all(queryPromises);
+    write("✅\n");
+
+    write("\n2/3 Running processor... ");
+    const processor = await loadProcessor(recipeFolder);
+    const schemaItems = await runProcessor({
+      processor,
+      queryResults,
+    });
+    writeln("✅\n");
+
+    writeln("Schema items:");
+    writeln(JSON.stringify(schemaItems, null, 2));
+    console.log("Schema:", recipe.schema);
+    console.log(
+      "Schema UID:",
+      getSchemaUid({
+        schema: recipe.schema,
+        resolver: recipe.resolver,
+        revokable: recipe.revokable,
+      })
+    );
+
+    write("\n3/3 Validating schema items against schema... ");
+    const schema = await validateSchemaItems({
+      schemaItems,
+      recipe,
+    });
+    writeln("✅\n");
+
+    console.log("💥 Done! Recipe is ready to be deployed.");
+  } catch (error) {
+    console.log("\n🛑 Run failed");
+    logError(error);
+  }
+}
+
+const program = new Command();
+program
+  .version("0.0.1")
+  .name("catts")
+  .description("Supports the development of C-ATTS recipes.")
+  .option(
+    "-e, --eth-address <address>",
+    "Ethereum address to use for queries. Defaults to the value of the USER_ETH_ADDRESS environment variable."
+  );
+
+program.hook("preAction", async (thisCommand) => {
+  // If -e option is set, override process.env.USER_ETH_ADDRESS
+  const ethAddressOption = thisCommand.opts().ethAddress;
+  if (ethAddressOption) {
+    process.env.USER_ETH_ADDRESS = ethAddressOption;
+  }
+
+  // Ensure USER_ETH_ADDRESS is set
+  if (!process.env.USER_ETH_ADDRESS) {
+    console.error(
+      "Error: userEthAddress needs to be set, either via the -e option or the USER_ETH_ADDRESS environment variable."
+    );
+    process.exit(1);
+  }
+});
+
+program
+  .command("query")
+  .argument("<recipeFolder>", "Path to the recipe folder.")
+  .option(
+    "-i, --index <index>",
+    "Index of query to run. Omit to run all queries"
+  )
+  .description("Fetch the query results from the specified recipe.")
+  .action(async (recipeFolder, options) => {
+    await queryCommand(recipeFolder, options);
+  });
+
+program
+  .command("run")
+  .argument("<recipeFolder>", "Path to the recipe folder.")
+  .description("Run the specified recipe.")
+  .action(async (recipeFolder) => {
+    await runCommand(recipeFolder);
+  });
+
+program.parse(process.argv);
---
+++ lib.ts
@@ -0,0 +1,214 @@
+import { SchemaEncoder } from "@ethereum-attestation-service/eas-sdk";
+import { getQuickJS } from "quickjs-emscripten";
+import { solidityPackedKeccak256 } from "ethers";
+import { z } from "zod";
+
+/**
+ *  Returns the input GraphQL query as-is. Placeholder for further GraphQL processing if needed.
+ * The use of "graphql('...')" helps editors and IDEs to correctly highlight the query.
+ */
+export function graphql(query: string) {
+  return query;
+}
+
+/**
+ * Defines the shape of query variables to be used in GraphQL queries.
+ */
+export type QueryVariables = {
+  [key: string]: QueryVariables | string | number;
+};
+
+/**
+ * Defines the components of a GraphQL query, including endpoint and variables.
+ */
+export type Query = {
+  endpoint: string;
+  query: string;
+  variables: QueryVariables;
+};
+
+/**
+ * Defines the structure of a recipe, including queries and output schema.
+ */
+export type Recipe = {
+  name: string;
+  displayName?: string;
+  description?: string;
+  keywords?: string[];
+  queries: Query[];
+  schema: string;
+  resolver: string;
+  revokable: boolean;
+};
+
+// Define the basic types
+const schemaValueBase = z.union([
+  z.string(),
+  z.boolean(),
+  z.number(),
+  z.bigint(),
+]);
+
+// Define the more complex types
+const schemaValueComplex = z.union([
+  z.record(z.unknown()), // Record<string, unknown>
+  z.array(z.record(z.unknown())), // Record<string, unknown>[]
+  z.array(z.unknown()), // unknown[]
+]);
+
+// Combine the base and complex schemas into a single SchemaValue schema
+const SchemaValue = z.union([schemaValueBase, schemaValueComplex]);
+
+// Define the SchemaItem schema
+const SchemaItem = z.object({
+  name: z.string(),
+  type: z.string(),
+  value: SchemaValue,
+});
+
+export type SchemaItem = z.infer<typeof SchemaItem>;
+
+function processVariables(variables: QueryVariables): QueryVariables {
+  const userEthAddress = process.env.USER_ETH_ADDRESS;
+
+  if (!userEthAddress) {
+    throw new Error("USER_ETH_ADDRESS is not set");
+  }
+
+  // Replaces placeholder tokens in the variables with actual userEthAddress
+  function replaceUserEthAddress(obj: QueryVariables): QueryVariables {
+    return Object.entries(obj).reduce<QueryVariables>((acc, [key, value]) => {
+      if (typeof value === "string" && value.includes("{user_eth_address}")) {
+        acc[key] = value.replace("{user_eth_address}", userEthAddress!);
+      } else if (typeof value === "object" && value !== null) {
+        acc[key] = replaceUserEthAddress(value);
+      } else {
+        acc[key] = value;
+      }
+      return acc;
+    }, {});
+  }
+
+  return replaceUserEthAddress(variables);
+}
+
+/**
+ * Fetches the result of a query from the GraphQl endpoint specified in the query.
+ *
+ * @returns The result of the query in JSON format.
+ */
+export async function fetchQuery(query: Query) {
+  try {
+    const variables = processVariables(query.variables);
+
+    const response = await fetch(query.endpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ query: query.query, variables }),
+    });
+
+    return response.json();
+  } catch (error) {
+    return error;
+  }
+}
+
+/**
+ * Runs the processor script against the query results and returns the result.
+ *
+ * @param processor The processor javascript code to be executed.
+ * @param queryResults An array of query results in JSON format.
+ *
+ * @returns The result of the processor script in JSON format. On success, the output should  match outputSchema specified in the recipe.
+ */
+export async function runProcessor({
+  processor,
+  queryResults,
+}: {
+  processor: string;
+  queryResults: any;
+}): Promise<SchemaItem[]> {
+  const QuickJS = await getQuickJS();
+  const vm = QuickJS.newContext();
+
+  try {
+    const queryResultRaw = vm.newString(JSON.stringify(queryResults));
+    vm.setProp(vm.global, "queryResultRaw", queryResultRaw);
+    queryResultRaw.dispose();
+
+    processor = `
+      let queryResult = JSON.parse(queryResultRaw).map((res) => res.data);
+      function process() {{
+        ${processor}
+      }}
+      process();
+    `;
+
+    const result = vm.evalCode(processor);
+    if (result.error) {
+      const error = vm.dump(result.error);
+      result.error.dispose();
+      throw error;
+    }
+
+    const value = vm.dump(result.value);
+    result.value.dispose();
+
+    const json = JSON.parse(value);
+
+    if (!Array.isArray(json)) {
+      throw new Error("Processor result must be an array");
+    }
+
+    if (json.length === 0) {
+      throw new Error("Processor returned an empty array");
+    }
+
+    let schemaItems: SchemaItem[];
+    try {
+      schemaItems = json.map((item: any) => SchemaItem.parse(item));
+    } catch (error) {
+      throw new Error("Invalid processor result");
+    }
+
+    return schemaItems;
+  } catch (error) {
+    throw error;
+  } finally {
+    vm.dispose();
+  }
+}
+
+/**
+ * Validates that the schema items match the schema specified in the recipe.
+ */
+export async function validateSchemaItems({
+  schemaItems,
+  recipe,
+}: {
+  schemaItems: SchemaItem[];
+  recipe: Recipe;
+}) {
+  const schemaEncoder = new SchemaEncoder(recipe.schema);
+  return schemaEncoder.encodeData(schemaItems);
+}
+
+/**
+ * An EAS schema UID is a hash of the schema, resolver and revokable flag.
+ */
+export function getSchemaUid({
+  schema,
+  resolver,
+  revokable,
+}: {
+  schema: string;
+  resolver: string;
+  revokable: boolean;
+}) {
+  return solidityPackedKeccak256(
+    ["string", "address", "bool"],
+    [schema, resolver, revokable]
+  );
+}
